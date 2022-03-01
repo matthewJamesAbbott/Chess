e7b907d6f5ea4b691c75143f437d87aca9d579d9
MatthewJamesAbbott <mattbachg@gmail.com>
2022-03-01 18:42:23 +1100

    The first commit of my terminal chess program writen in C++.
    displays unicode board with pieces and allows legal moves.
    
    still needed to be done is castling and en passant.
    
    network modules have been added but not integrated yet.

--- main.cpp
+++ main.cpp
@@ -11,19 +11,70 @@ int main() {
     int x, xa;
     char y, ya;
     while(true) {
-        std::cout << "Please enter number for piece you wish to move" << std::endl;
-        std::cin >> x;
-        if(x == 10)
-            break;
-        std::cout << "Please enter letter for piece you wish to move" << std::endl;
-        std::cin >> y;
-        std::cout << "Please enter number for were you wish piece to move too" << std::endl;
-        std::cin  >> xa;
-        std::cout << "Please enter letter for were you wish piece to move too" << std::endl;
-        std::cin >> ya;
+        while(true){
+            std::cout << "Please enter number for piece you wish to move or 9 too exit" << std::endl;
+            std::cin >> x;
+            if(x == 1 || x == 2 || x == 3 || x == 4 || x == 5 || x == 6 || x == 7 || x == 8){
+                break;
+            }
+            else if(x == 9)
+                return 0;
+            else {
+                std::cout << "You have entered an illegal character please try again" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(1, '\n');
+                x = 0;
+            }
+        }
+        while(true){
+            std::cout << "Please enter letter for piece you wish to move or 9 too exit" << std::endl;
+            std::cin >> y;
+            if(y == 'a' || y == 'A' || y == 'b' || y == 'B' || y == 'c' || y == 'C' || y == 'd' || y == 'D'
+                || y == 'e' || y == 'E' || y == 'f' || y == 'F' || y == 'g' || y == 'G' || y == 'h' || y == 'H'){
+                break;
+            }
+            else if(y == '9')
+                return 0;
+            else {
+                std::cout << "You have entered an illegal character please try again" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(1, '\n');
+                y = 0;
+            }
+        }
+        while(true){
+            std::cout << "Please enter number for were you wish piece to move too or 9 too exit" << std::endl;
+            std::cin >> xa;
+            if(xa == 1 || xa == 2 || xa == 3 || xa == 4 || xa == 5 || xa == 6 || xa == 7 || xa == 8){
+                break;
+            }
+            else if(xa == 9)
+                return 0;
+            else {
+                std::cout << "You have entered an illegal character please try again" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(1, '\n');
+                xa = 0;
+            }
+        }
+        while(true) {
+            std::cout << "Please enter letter for were you wish piece to move too or 9 too exit" << std::endl;
+            std::cin >> ya;
+            if (ya == 'a' || ya == 'A' || ya == 'b' || ya == 'B' || ya == 'c' || ya == 'C' || ya == 'd' || ya == 'D'
+                || ya == 'e' || ya == 'E' || ya == 'f' || ya == 'F' || ya == 'g' || ya == 'G' || ya == 'h' ||
+                ya == 'H') {
+                break;
+            } else if (ya == '9')
+                return 0;
+            else {
+                std::cout << "You have entered an illegal character please try again" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(1, '\n');
+                ya = 0;
+            }
+        }
         test.movePiece(x, y, xa, ya);
         test.printBoardToTerminal();
-
-
     }
+    return 0;
 }
