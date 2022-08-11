97d3625ba91d067f9ca96caea397aefd7206e4e0
Matthew Abbott <98388418+matthewJamesAbbott@users.noreply.github.com>
2022-08-11 16:30:35 +1000

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -6,21 +6,21 @@
 #include <utility>
 #include "Board.h"
 
-Board::Board()= default;
+Board::Board()= default; // Board default contructor
 
-Board::Board(Board const &copy){
+Board::Board(Board const &copy){ // Board copy constructor
     for(int e = 0; e < 8; e++){
         for(int i = 0; i < 8; i++){
             board[e][i] = copy.board[e][i];
         }
     }
 }
-Board::~Board()= default;
+Board::~Board()= default; // Board deconstructor
 
 std::string Board::returnSquare(int x, int y){
     return board[x][y];
 }
 
-void Board::setSquare(int x, int y, std::string piece){
+void Board::setSquare(int x, int y, std::string piece){ // insert string representation of piece into 2D array board
     board[x][y] = std::move(piece);
 }
